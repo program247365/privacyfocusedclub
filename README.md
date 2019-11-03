2bf1b28c55e511dd39069af4401d91e3c31c82c3
Kevin B. Ridgway <kridgway@gmail.com>
2019-11-02 23:11:51 -0400

    Adding license.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Privacy Focused Club
+
+> For those interested in finding privacy focused services and websites to use on the web.
--- components/nav.js
+++ components/nav.js
@@ -2,8 +2,7 @@ import React from 'react'
 import Link from 'next/link'
 
 const links = [
-  { href: 'https://zeit.co/now', label: 'ZEIT' },
-  { href: 'https://github.com/zeit/next.js', label: 'GitHub' }
+  { href: 'https://kevinridgway.com', label: 'A joint from Kevin B. Ridgway' }
 ].map(link => {
   link.key = `nav-link-${link.href}-${link.label}`
   return link
