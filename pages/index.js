975e88265eb5be87be79d84cc3310611f4621930
Kevin B. Ridgway <kridgway@gmail.com>
2019-11-02 23:25:17 -0400

    First three products.

--- pages/index.js
+++ pages/index.js
@@ -17,9 +17,33 @@ const Home = () => (
       For those interested in finding privacy focused services and websites to use on the web.
       </p>
       <p className='description'>
-      Coming soon...
+      As the index gets built, here are some great privacy-focused products to get you started...
       </p>
-    </div>
+      <div className='row'>
+       <a href='https://simpleanalytics.com/' className='card'>
+         <h3>Simple Analytics &rarr;</h3>
+         <p> Simple, clean, and
+         friendly analytics. We show you the essentials: page views, referrers, top pages, and screen sizes. We enrich this data with useful info like original Tweets. We don't use cookies or collect any personal data. So no cookie banners, GDPR, CCPA, or PECR to worry about.
+         </p>
+       </a>
+       <a href='https://duckduckgo.com' className='card'>
+         <h3>DuckDuckGo &rarr;</h3>
+         <p>The search engine that doesn't track you. Our privacy policy is simple: we don’t collect
+         or share any of your personal information. We don’t store your search history. We therefore have nothing
+         to sell to advertisers that track you across the Internet. Other search engines track your searches even when you’re
+         in private browsing mode. We don’t track you — period.</p>
+       </a>
+       <a
+         href='https://www.mozilla.org/en-US/firefox/privacy/products/'
+         className='card'
+       >
+         <h3>Firefox Browser &rarr;</h3>
+         <p>
+
+         Tracking has become an epidemic online: companies follow every move, click and purchase, collecting data to predict and influence what you’ll do next. We think that’s a gross invasion of your privacy. That’s why Firefox mobile and desktop browsers have Enhanced Tracking Protection on by default. </p>
+       </a>
+     </div>
+   </div>
 
     <style jsx>{`
       .hero {
